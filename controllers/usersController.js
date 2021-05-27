fe379c302f4436c84f06ba5c6cecc6b349c170f8
anggidast <adastariana@gmail.com>
2021-05-27 23:47:49 +0700

    add User model and User routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,13 @@
 const { User } = require(`../models`);
 
 class Controller {
+  static register(req, res) {
+    console.log(req.body);
+    User.create(req.body)
+      .then((user) => res.status(201).json({ success: true, user: user }))
+      .catch((err) => res.status(500).json({ success: false, message: err }));
+  }
+
   static login(req, res) {
     User.findOne({
       where: { email: req.body.email, password: req.body.password },
@@ -9,13 +16,6 @@ class Controller {
       .catch(() =>
         res.status(404).json({ success: false, message: 'User not found' })
       );
-  }
-
-  static register(req, res) {
-    console.log(req.body);
-    User.create(req.body)
-      .then((user) => res.status(201).json({ success: true, user: user }))
-      .catch((err) => res.status(500).json({ success: false, message: err }));
   }
 }
 

5c68b85215796fc570bd2996e50534f87b986667
budigk <budigk@gmail.com>
2021-06-03 20:03:35 +0700

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require('express');
 const errHandler = require('./middlewares/errHandler.js');
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 const routes = require('./routes/route.js');
 
+app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 

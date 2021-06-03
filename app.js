5d2668c3047661c166f91dd4f217fffc3ac46045
Karen <karen@KD-MM.local>
2021-06-03 17:35:03 +0700

    Commit - 11

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const routes = require('./routes/route-main.js')
-const PORT = 3000
 const errorHandler = require('./helpers/errorHandler.js')
 
+const app = express()
+const PORT = 3000
+
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(routes)
-
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Listening at Localhost:${PORT}`))

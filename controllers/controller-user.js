5541e70d38d61cccacb634d3f8264188a798b323
Karen <karen@KD-MM.local>
2021-06-06 16:43:48 +0700

    6 Jun - 16.43

--- controllers/controller-user.js
+++ controllers/controller-user.js
@@ -4,7 +4,62 @@ const { User, Todo } = require('../models')
 const { comparePassword } = require('../helpers/bcrypt.js')
 const { generateToken } = require('../helpers/jwt.js')
 
+// GOOGLE LOG-IN VERIFICATION -- OAUTH:
+const {OAuth2Client} = require('google-auth-library')
+
 class ControllerUser {
+
+    static googleLogin (req, res, next) {
+        // proses verfikasi access_token Google oleh Google
+            // karena Google punya SECRET KEY internal sendiri untuk verify token
+            // dan ini pakai APInya Google, maka harus Google yg verify bener nggak tuh token di-issue/generate oleh internalnya dia
+        
+        const client = new OAuth2Client(process.env.CLIENT_ID)
+        // CLIENT_ID -> yg tadi didapat daftar Credentials di Google
+        // bersifat rahasia, makanya dimasukkan ke .ENV
+
+        let emailToRegister = ''
+        const {google_access_token} = req.headers
+
+        client.verifyIdToken({
+            idToken: google_access_token,
+            audience: process.env.CLIENT_ID
+        })
+            .then(ticket => {
+                return ticket.getPayload()
+            })
+            .then(payload => {
+                console.log(payload)
+                emailToRegister = payload.email
+                return User.findOne({where: {email: payload.email}}) // check User dengan email ini sudah terdaftar belum di DB kita
+            })
+            .then(user => {
+                if (!user) { // kalau (null) => BELUM TERDAFTAR: maka kita Create manual, pakai email yg dari payload tadi
+                    return User.create({
+                        email: emailToRegister,
+                        password: Math.random().toString(36).slice(-8) // random password generator
+                    })
+                } else { // kalau SUDAH TERDAFTAR: langsung aja ke proses berikutnya
+                    return user
+                }
+            })
+            .then(user => { // di sini kita udah punya EMAIL dan PASSWORD yg OK untuk didaftarkan
+                // sekarang create JWTnya, sama seperti pas login manual (di bawah)
+                const token = generateToken({
+                    id: user.id,
+                    email: user.email
+                })
+                console.log(token)
+                return res.status(200).json({
+                    accesstoken: token
+                })
+            })
+            .catch(err => {
+                // next(err)
+                console.log(err)
+            })
+
+    }
 
     static register (req, res, next) {
         const input = {
--- routes/route-users.js
+++ routes/route-users.js
@@ -6,4 +6,7 @@ const ControllerUser = require('../controllers/controller-user.js')
 route.post('/register', ControllerUser.register)
 route.post('/login', ControllerUser.login)
 
+// ROUTER FOR LOG-IN WITH GOOGLE OAUTH:
+route.post('/googleLogin', ControllerUser.googleLogin)
+
 module.exports = route
